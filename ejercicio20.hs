44531abd1123020dd386cf7282110591d0979c3a
FrancoT67 <francotoledo1235>
2023-03-21 20:29:03 -0300

    .

---
+++ ejercicio20.hs
@@ -0,0 +1,2 @@
+--20 calcular año bisiesto o no
+esBisiesto a= (mod a 4 == 0)&&(mod a 400 == 0)&&(mod a 100 /= 0)
---
+++ ejercicio21.hs
@@ -0,0 +1,2 @@
+--21 diferencia entre el maximo y el minimo
+dispersion a b c =max(max(a b),c)-min(min(a b),c)
