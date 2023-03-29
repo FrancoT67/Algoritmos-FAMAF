5726ab9a5a4aaf0d8826b69d63390a0eac7fec4a
FrancoT67 <91621949+FrancoT67@users.noreply.github.com>
2023-03-29 12:54:13 -0300

    Add files via upload

---
+++ ejercicio4.hs
@@ -0,0 +1,15 @@
+--a
+sumar1 :: Int -> [Int] -> [Int]
+
+sumar1 n xs | xs == [] =[] |xs /= []  = (head xs)+n : sumar1 n (tail xs)
+
+--b
+duplica :: [Int] -> [Int]
+
+duplica xs | xs == [] =[] |xs /= []  = (head xs)*2 : duplica (tail xs)
+
+--c
+multiplica :: Int -> [Int]-> [Int]
+
+multiplica n xs | xs == [] =[] |xs /= []  = (head xs)*n : multiplica n (tail xs)
+
---
+++ ejercicio5.hs
@@ -0,0 +1,17 @@
+--a
+todosMenores10 :: [Int] -> Bool
+
+todosMenores10 xs | length xs == 0 = True 
+				  | (head xs) < 10 = todosMenores10 (tail xs) 
+                  | (head xs) >= 10 = False   
+
+hay0 :: [Int] -> Bool
+
+hay0 xs | length xs == 0 = False 
+	    | (head xs) ==0 = True 
+        | (head xs) /=0 = hay0 (tail xs)   
+
+sumar :: [Int] -> Int
+
+sumar xs | length xs == 0 = 0
+	   | length xs /= 0 = head(xs)+ sumar (tail xs)
