e1a4a4a8e803676d67392cb450699cb42c0df2b1
FrancoT67 <francotoledo1235>
2023-09-04 21:18:07 -0300

    FIX proyecto 1

--- algoritmos y estructuras de datos 1/Proyecto1/proyecto1.hs
+++ algoritmos y estructuras de datos 1/Proyecto1/proyecto1.hs
@@ -1,6 +1,11 @@
 -- Ejercicios
 -- 1
 -- a) esCero :: Int -> Bool, que verifica si un entero es igual a 0.
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use foldr" #-}
+{-# HLINT ignore "Use map" #-}
+{-# HLINT ignore "Eta reduce" #-}
 
 esCero :: Int -> Bool
 esCero x = x == 0
@@ -42,7 +47,7 @@ paraTodo (x : xs)
   | x = paraTodo xs
   | not x = False
 
-{- 
+{-
 paraTodo [True,True,True,True] = True
 paraTodo [True,True,False,True] = False
 -}
@@ -105,8 +110,7 @@ paratodo' (x : xs) funcion = funcion x && paratodo' xs funcion
 {-
 paratodo' [0] esCero = True
 paratodo' ['a'] esVocal = True
--} 
- 
+-}
 
 -- b) existe' :: [a] -> (a -> Bool) -> Bool, dada una lista xs de tipo [a] y un predicado t :: a -> Bool, determina si algún elemento de xs satisface el predicado t.
 existe' :: [a] -> (a -> Bool) -> Bool
@@ -120,7 +124,7 @@ existe' ['b','c'] esVocal == False
 existe' [1,2,0] esCero == True
 existe' [1,2,3] esCero == False
 existe' ['b','a'] esVocal == True
--} 
+-}
 
 -- c) sumatoria’ :: [a] -> (a -> Int) -> Int, dada una lista xs de tipo [a] y una función t :: a -> Int (toma elementos de tipo a y devuelve enteros), calcula la suma de los valores que resultan de la aplicación de t a los elementos de xs.
 sumatoria' :: [a] -> (a -> Int) -> Int
@@ -130,7 +134,7 @@ sumatoria' (x : xs) t = t x + sumatoria' xs t
 {-
 sumatoria' [1,2,3] valorAbsoluto = 6
 sumatoria' [1,2,3] factorial = 9
--}  
+-}
 
 -- d) productoria’ :: [a] -> (a -> Int) -> Int, dada una lista de xs de tipo [a] y una función t :: a -> Int, calcula el producto de los valores que resultan de la aplicación de t a los elementos de xs.
 productoria' :: [a] -> (a -> Int) -> Int
@@ -140,7 +144,7 @@ productoria' (x : xs) t = t x * productoria' xs t
 {-
 productoria' [1,2,3,4,5,6,7] valorAbsoluto = 5040
 productoria' [4,5] falctorial = 2880
--}  
+-}
 
 -- 5. Definí nuevamente la función paratodo, pero esta vez usando la función paratodo' (Sin recursión, ni análisis por casos!).
 
@@ -150,7 +154,7 @@ paratodo'' xs = paratodo' xs id
 {-
 paratodo'' [True, True] = True
 paratodo'' [True, False] = False
--} 
+-}
 
 -- 6. Utilizando las funciones del ejercicio 4, programá las siguientes funciones por composición, sin usar recursión ni análisis por casos.
 -- a) todosPares :: [Int] -> Bool verifica que todos los números de una lista sean pares.
@@ -158,9 +162,9 @@ todosPares :: [Int] -> Bool
 todosPares xs = paratodo' xs even
 
 {-
-todosPares [1,2,3,4,5,6] = False 
+todosPares [1,2,3,4,5,6] = False
 todosPares [2,4,6] = True
--} 
+-}
 
 -- b) hayMultiplo :: Int -> [Int] -> Bool verifica si existe algún número dentro del segundo parámetro que sea múltiplo del primer parámetro.
 
@@ -173,7 +177,7 @@ hayMultiplo n xs = existe' xs (multiplo n)
 {-
 hayMultiplo 2 [1,2,3,4,5,6] = True
 hayMultiplo 7 [1,2,3,4,5,6] = False
--} 
+-}
 
 -- c) sumaCuadrados :: Int -> Int, dado un número no negativo n, calcula la suma de los primeros n cuadrados, es decir <∑ i : 0 ≤ i < n : i^2>.
 -- Ayuda: En Haskell se puede escribir la lista que contiene el rango de números entre n y m como [n..m].
@@ -181,10 +185,10 @@ hayMultiplo 7 [1,2,3,4,5,6] = False
 sumaCuadrados :: Int -> Int
 sumaCuadrados n = sumatoria' [0 .. n] (^ 2)
 
-{- 
+{-
 sumaCuadrados 3 = 14
 sumaCuadrados 6 = 91
--} 
+-}
 
 -- d) Programar la fución existeDivisor::Int-> [Int] -> Bool, que dado en entero N y una lista LS, devuelve True si y solo si, existe algún elemento en LS que divida a N.
 
@@ -194,10 +198,10 @@ divisor n m = n `mod` m == 0
 existeDivisor :: Int -> [Int] -> Bool
 existeDivisor n ls = existe' ls (divisor n)
 
-{- 
+{-
 existeDivisor 6 [1,2,3,4,5,6] =True
 existeDivisor 12 [1,2,3,4,5,6] = True
--} 
+-}
 
 -- e) Utilizando la función del apartado anterior, definí la función esPrimo:: Int -> Bool, que dado un entero n, devuelve True si y solo si n es primo.
 --  Ayuda: En Haskell se puede escribir la lista que contiene el rango de números entre n y m como [n..m].
@@ -205,14 +209,15 @@ existeDivisor 12 [1,2,3,4,5,6] = True
 esPrimo :: Int -> Bool
 esPrimo n = not (existeDivisor n [2 .. (n - 1)])
 
-{- 
+{-
 esPrimo 13 = True
 esPrimo 12 = False
--} 
+-}
 
 -- f ) ¿Se te ocurre cómo redefinir factorial (ej. 2d) para evitar usar recursión?
 factorial' :: Int -> Int
 factorial' n = productoria [1 .. n]
+
 {-
 factorial' 4 = 24
 factorial' 5 = 120
@@ -224,6 +229,7 @@ primos x | esPrimo x = x | not (esPrimo x) = 1
 
 multiplicaPrimos :: [Int] -> Int
 multiplicaPrimos m = productoria' m primos
+
 {-
 multiplicaPrimos [1,2,3,4,5] = 30
 multiplicaPrimos [1,2,3,4,5,6,7,8,9,10] = 210
@@ -234,17 +240,16 @@ multiplicaPrimos [1,2,3,4,5,6,7,8,9,10] = 210
 
 fib :: Int -> Int
 fib n
-  | n<0 = 0
-  | n>=0 && n<=2 = n
+  | n < 0 = 0
+  | n >= 0 && n <= 2 = n
   | otherwise = fib (n - 1) + fib (n - 2)
 
-
 verifica :: Int -> [Int]
-verifica 0 = 0:1:[]
-verifica n = verifica (n-1)++[fib n] 
+verifica 0 = [0, 1]
+verifica n = verifica (n - 1) ++ [fib n]
 
 esFib :: Int -> Bool
-esFib n = existe' (verifica n) (==n)
+esFib n = existe' (verifica n) (== n)
 
 {-
 verifica 10 =[89,55,34,21,13,5,3,2,1]
@@ -252,12 +257,11 @@ esFib 11= False
 esFib 8= True
 -}
 
-
---i) Utilizando la función del apartado anterior, definí la función todosFib :: [Int] -> Bool, que dada una lista xs de enteros, devuelva si todos los elementos de la lista pertenecen (o no) a la sucesión de Fibonacci.
+-- i) Utilizando la función del apartado anterior, definí la función todosFib :: [Int] -> Bool, que dada una lista xs de enteros, devuelva si todos los elementos de la lista pertenecen (o no) a la sucesión de Fibonacci.
 
 todosFib :: [Int] -> Bool
-todosFib []=True
-todosFib (x:xs) = esFib x && todosFib xs
+todosFib [] = True
+todosFib (x : xs) = esFib x && todosFib xs
 
 {-
 todosFib [8,5,3,2,1] = True
@@ -266,7 +270,7 @@ todosFib [8,5,4,2,1] = False
 
 -- 7. Indagá en Hoogle sobre las funciones map y filter. También podes consultar su tipo en ghci con el comando :t
 -- ¿Qué hacen estas funciones?
-{- 
+{-
 La función map lo que hace es aplicar una función a cada elemento de una lista y retornar una lista con los resultados obtenidos de haber aplicado dicha función.
 -}
 -- ¿A qué equivale la expresión map succ [1, -4, 6, 2, -8], dde succ n = n+1?
@@ -278,20 +282,22 @@ La expresión equivale a sumarle 1 a cada elemento de la lista [1, -4, 6, 2, -8]
 La expresion equivale a eliminar todos los elementos que son menores que 0
 -}
 
---8. Programá una función que dada una lista de números xs, devuelve la lista que resulta de duplicar cada valor de xs.
+-- 8. Programá una función que dada una lista de números xs, devuelve la lista que resulta de duplicar cada valor de xs.
 
 -- a) Definila usando recursión.
-duplica :: [Int]-> [Int]
-duplica []=[]
-duplica (x:xs) = (x*2):duplica xs
+duplica :: [Int] -> [Int]
+duplica [] = []
+duplica (x : xs) = (x * 2) : duplica xs
+
 {-
 duplica [1, -4, 6, 2, -8] = [2,-8,12,4,-16]
 duplica [2, -5, 7, 3, -9] = [4,-10,14,6,-18]
 -}
 
 -- b) Definila utilizando la función map.
-duplica' :: [Int]-> [Int]
-duplica' n = map (*2) n
+duplica' :: [Int] -> [Int]
+duplica' n = map (* 2) n
+
 {-
 duplica' [1, -4, 6, 2, -8] = [2,-8,12,4,-16]
 duplica' [2, -5, 7, 3, -9] = [4,-10,14,6,-18]
@@ -302,8 +308,9 @@ duplica' [2, -5, 7, 3, -9] = [4,-10,14,6,-18]
 -- a) Definila usando recursión.
 soloPrimos :: [Int] -> [Int]
 soloPrimos [] = []
-soloPrimos (x:xs) | esPrimo x = x:soloPrimos xs
-                  | not (esPrimo x) =[]++soloPrimos xs
+soloPrimos (x : xs)
+  | esPrimo x = x : soloPrimos xs
+  | not (esPrimo x) = soloPrimos xs
 
 {-
 soloPrimos [1,2,3,4,5,6,7,8,9] = [1,2,3,5,7]
@@ -322,20 +329,22 @@ soloPrimos' [2,5,7,8,21] = [2,5,7]
 
 -- Se la puede mejorar usando la función creada en el ej 9a, ésta sería :
 productoria'' :: [Int] -> Int
-productoria'' n = productoria (soloPrimos' n ) 
+productoria'' n = productoria (soloPrimos' n)
+
 {-
 productoria'' [1,2,3,4,5,6,7,8,9] = 210
 productoria'' [7,8,9] = 7
 -}
-
 
 -- 10. La función primIgualesA toma un valor y una lista, y calcula el tramo inicial más largo de la lista cuyos elementos son iguales a ese valor.
 
 -- a) Programá primIgualesA por recursión.
 primIgualesA :: Eq a => a -> [a] -> [a]
 primIgualesA n [] = []
-primIgualesA n (x:xs)| n == x = [x]++primIgualesA n xs 
-                     | x /= n = []
+primIgualesA n (x : xs)
+  | n == x = x : primIgualesA n xs
+  | x /= n = []
+
 {-
 primIgualesA 3 [3,4,3,4]= [3]
 primIgualesA 3 [4,3,3,4]= []
@@ -345,7 +354,8 @@ primIgualesA 'a' "baaAAA" = ""
 
 -- b) Programá nuevamente la función utilizando takeWhile.
 primIgualesA' :: Eq a => a -> [a] -> [a]
-primIgualesA' n xs= takeWhile (==n) xs 
+primIgualesA' n xs = takeWhile (== n) xs
+
 {-
 primIgualesA' 3 [3,4,3,4]= [3]
 primIgualesA' 3 [4,3,3,4]= []
@@ -353,12 +363,13 @@ primIgualesA' 'a' "aaaAAA" = "aaa"
 primIgualesA' 'a' "baaAAA" = ""
 -}
 
---11. La función primIguales toma una lista y devuelve el mayor tramo inicial de la lista cuyos elementos son todos iguales entre sí.
+-- 11. La función primIguales toma una lista y devuelve el mayor tramo inicial de la lista cuyos elementos son todos iguales entre sí.
 
 -- a) Programá primIguales por recursión.
 primIguales :: Eq a => [a] -> [a]
 primIguales [] = []
-primIguales (x:y:xs) | x == y = [x]++[y]++primIguales xs | x /= y = x:[] 
+primIguales (x : y : xs) | x == y = [x] ++ [y] ++ primIguales xs | x /= y = [x]
+
 {-
 primIguales "aDaDa" = "a"
 primIguales "aaaDa" = "aaa"
@@ -366,9 +377,9 @@ primIguales "aaaDa" = "aaa"
 
 -- b) Usá cualquier versión de primIgualesA para programar primIguales. Está permitido dividir en casos, pero no usar recursión.
 primIguales' :: Eq a => [a] -> [a]
-primIguales' (x:xs) = primIgualesA' x (x:xs)
+primIguales' (x : xs) = primIgualesA' x (x : xs)
+
 {-
 primIguales' "aDaDa" = "a"
 primIguales' "aaaDa" = "aaa"
 -}
-
