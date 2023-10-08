4bd2c8f71a251e4f808322b2b16a96b12e96618a
FrancoT67 <francotoledo1235>
2023-10-08 20:20:20 -0300

    agregado de archivos .json

--- algoritmos y estructuras de datos 1/Proyecto3/Ej6/6.a/entradas.c
+++ algoritmos y estructuras de datos 1/Proyecto3/Ej6/6.a/entradas.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 // Funciones Originales.
 int pedirEntero(char name);
-int main(void){
-    int m=0;
+int main(void)
+{
+    int m = 0;
 
-    m=pedirEntero('m');
-    printf("M vale:%d\n",m);
+    m = pedirEntero('m');
+    printf("M vale:%d\n", m);
     return 0;
 }
 
-int pedirEntero(char name){
-    int n=0;
-    printf("Ingrese el entero para la variable %c:",name);
-    scanf("%d",&n);
-    return n;
+int pedirEntero(char name)
 
+{
+    int n = 0;
+    printf("Ingrese el entero para la variable %c:", name);
+    scanf("%d", &n);
+    return n;
 }
