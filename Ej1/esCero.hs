0c4db538d5e1b61763a863758d8b53678f0b7906
FrancoT67 <francotoledo1235>
2024-03-15 02:35:36 -0300

    en equipo con Delfi hasta el 4, falta 5-11

--- Ej1/esCero.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE NPlusKPatterns #-}
-
-esCero :: Int -> Bool
-esCero x = x == 0
-{-
-Ejemplos de ejecucion:
-esCero 2 --->False
-esCero 0 --->True
--}
--- Ej1/esPositivo.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE NPlusKPatterns #-}
-
-esPositivo :: Int -> Bool
-esPositivo x = x >= 0
-
-{-
- esPositivo (-2) ---> False
- esPositivo 2 ---> True
--}
