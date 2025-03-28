1375ac5f03b0be8aa0849bf200b178d23a3b0301
FrancoT67 <francotoledo1235@gmail.com>
2025-03-28 15:50:34 -0300

    ej0-lab1

--- algoritmos y estructuras 2/lab01/ej0/fixstring.c
+++ algoritmos y estructuras 2/lab01/ej0/fixstring.c
@@ -6,31 +6,44 @@
 unsigned int fstring_length(fixstring s)
 {
 
-    unsigned int res = 0, i = 0;
+    unsigned int i = 0;
 
-    while (s[i] != '\0' && i < FIXSTRING_MAX)
+    while (i < FIXSTRING_MAX && s[i] != '\0')
     {
-        if (s[i] == '\0')
-        {
-            res = i;
-        }
-        i = +1;
+
+        i = i + 1;
     }
-    return res;
+    assert(i < FIXSTRING_MAX);
+    return i;
 }
 
 bool fstring_eq(fixstring s1, fixstring s2)
 {
-    /*
-     * COMPLETAR
-     *
-     */
+    bool res = true;
+
+    unsigned int long_s1 = fstring_length(s1);
+    unsigned int long_s2 = fstring_length(s2);
+
+    if (long_s1 == long_s2)
+    {
+        for (int i = 0; i < long_s1; i++)
+        {
+            res = s1[i] == s2[i];
+        }
+    }
+    else
+    {
+        res = false;
+    }
+
+    return res;
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2)
 {
-    /*
-     * COMPLETAR
-     *
-     */
+    bool res = true;
+    int tam1 = fstring_length(s1);
+    int tam2 = fstring_length(s2);
+
+    return res;
 }
