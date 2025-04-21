c9c1c0e8619bdc1251c9475df33b4567b0b9855b
FrancoT67 <francotoledo1235>
2025-04-21 16:32:22 -0300

    ej1 terminado

--- algoritmos y estructuras 2/lab01/ej1/sort.c
+++ algoritmos y estructuras 2/lab01/ej1/sort.c
@@ -6,13 +6,26 @@
 #include "sort_helpers.h"
 #include "sort.h"
 
+static void insert(int a[], unsigned int i, unsigned int length)
+{
 
-static void insert(int a[], unsigned int i, unsigned int length) {
-    /* needs implementation */
+    while (i > 0 && goes_before(a[i], a[i - 1]) && i < length)
+    {
+        swap(a, i, i - 1);
+        i--;
+    }
 }
 
-void insertion_sort(int a[], unsigned int length) {
-    for (unsigned int i = 1; i < length; ++i) {
+void insertion_sort(int a[], unsigned int length)
+{
+    for (unsigned int i = 1; i < length; ++i)
+    {
         insert(a, i, length);
+
+        // Parte B: imprimir el arreglo en cada paso
+        array_dump(a, length);
+
+        // Parte C: verificar invariante: a[0..i] está ordenado
+        assert(array_is_sorted(a, i));
     }
 }
