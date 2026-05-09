c588dc8e10e022f7c52abf5d21303f381871b997
FrancoT67 <francotoledo1235>
2026-05-09 12:28:00 -0300

    ej0 ok

--- algoritmos2/lab01/ej0/fixstring.c
+++ algoritmos2/lab01/ej0/fixstring.c
@@ -3,24 +3,48 @@
 
 #include "fixstring.h"
 
-unsigned int fstring_length(fixstring s) {
-    /*
-     * COMPLETAR
-     *
-     */
+unsigned int fstring_length(fixstring s)
+{
+    unsigned int i = 0;
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    return i;
 }
 
-bool fstring_eq(fixstring s1, fixstring s2) {
-    /*
-     * COMPLETAR
-     *
-     */
+bool fstring_eq(fixstring s1, fixstring s2)
+{
+    unsigned int i = 0;
+    bool res = true;
+    while (s1[i] != '\0' && s2[i] != '\0')
+    {
+        if (s1[i] != s2[i])
+        {
+            res = false;
+        }
+        i++;
+    }
+    /* si uno terminó antes que el otro, no son iguales */
+    if (s1[i] != s2[i])
+    {
+        res = false;
+    }
+    return res;
 }
+bool fstring_less_eq(fixstring s1, fixstring s2)
+{
+    unsigned int i = 0;
+    bool res = false;
 
-bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /*
-     * COMPLETAR
-     *
-     */
+    while (s1[i] != '\0' &&
+           s2[i] != '\0' &&
+           s1[i] == s2[i])
+    {
+        i++;
+    }
+
+    res = (s1[i] <= s2[i]);
+
+    return res;
 }
-
